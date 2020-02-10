5f4e0efafe9023d886de9256176624d375d93bc3
KilliannH <killiannhervagault@gmail.com>
2020-02-10 14:00:33 +0100

    understanding states

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,23 @@ class Person
 // new keyword for instances is not required.
 //MyApp() is an instance of MyApp class, we need the parentheses so Dart will not consider it as an argument type.
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+
+  var questionIndex = 0;
 
   void answerQuestion() {
+    setState(() {
+      questionIndex += 1;
+    });
     print('Answer chosen !');
   }
 
@@ -41,7 +55,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question'),
+            Text(questions[questionIndex]),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
             RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
             RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion)
@@ -66,3 +80,15 @@ class MyApp extends StatelessWidget {
 // All classes have to work as a Standalone.
 
 // the callback for onPressed is executed (we don't pass the function with parentheses).
+
+
+// Here we try to change the state of a widget in a stateless Widget...
+
+// A Stateful widget is a combination of 2 classes
+
+// Typically, a widget state class will be the name of the class + State and it will extend a state.
+
+// The Widget that extends the Stateful Widget will override createSTate() that returns the WidgetClassState
+// When a state has changed, we call setState() and we pass the changed value in this function.
+// under the hood, the build method from the stateful widget is called again.
+
